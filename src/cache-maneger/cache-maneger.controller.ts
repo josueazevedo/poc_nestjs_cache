4117e45ee00a5e9adb020c50b906fe2a9e4f0de6
Josué Azevedo <josue-azevedo@hotmail.com>
2024-03-11 21:52:54 -0300

    Add cache in GET resource

--- src/cache-maneger/cache-maneger.controller.ts
+++ src/cache-maneger/cache-maneger.controller.ts
@@ -6,13 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CacheManegerService } from './cache-maneger.service';
 import { UserDto } from './dto/user.dto';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('cache-maneger')
 export class CacheManegerController {
   constructor(private readonly cacheManegerService: CacheManegerService) {}
+
+  @Get('get-cache')
+  @UseInterceptors(CacheInterceptor)
+  getCache() {
+    console.log('method executed');
+
+    return {
+      message: 'Data in cache: ' + new Date().toLocaleTimeString(),
+    };
+  }
 
   @Post()
   create(@Body() userDto: UserDto) {
